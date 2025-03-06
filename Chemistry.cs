9b3dc726c8ad10bc3f5c1a320af94b7dde0533c3
ModxVoldHunter <65139923+ModxVoldHunter@users.noreply.github.com>
2025-03-06 22:22:15 +0000

    Pending changes exported from your codespace

---
+++ Chemistry.cs
@@ -0,0 +1,15 @@
+﻿namespace FullDiveSDK
+{
+    public class Chemistry
+    {
+        public int GetTemperature { get; set; }
+        public int GetPressure { get; set; }
+        public int GetVolume { get; set; }
+        public Chemistry()
+        {
+            GetTemperature = 0;
+            GetPressure = 0;
+            GetVolume = 0;
+        }
+    }
+}
---
+++ MainClass.cs
@@ -0,0 +1,53 @@
+﻿namespace FullDiveSDK
+{
+    public class MainClass
+    {
+        public class PlayerVelocity : Velocity
+        {
+            Velocity Velocity { get; set; }
+
+            public static readonly PlayerVelocity Instance = new PlayerVelocity();
+        }
+        public static string? GetWorld { get; set; }
+        public static string? WindowHeight { get; set; }
+        public static string? WindowWidth { get; set; }
+        public static object? Player { get; set; }
+        public static string GetVersion(int Version)
+        {
+            return GetVersion(Version);
+        }
+        public static string GetApplication(string AppName, int AppHeight, int AppWidth)
+        {
+            return GetApplication(AppName, AppHeight, AppWidth);
+        }
+        public static int GetObjectXYZ(int X, int Y, int Z)
+        {
+            return GetObjectXYZ(X, Y, Z);
+        }
+        public static int GetObjectXYZ(double X, double Y, double Z)
+        {
+            return GetObjectXYZ(X, Y, Z);
+        }
+        public static int GetObjectRotation(int X, int Y, int Z)
+        {
+            return GetObjectRotation(X, Y, Z);
+        }
+        public static int GetObjectRotation(double X, double Y, double Z)
+        {
+            return GetObjectRotation(X, Y, Z);
+        }
+        public static int GetObjectScale(int X, int Y, int Z)
+        {
+            return GetObjectScale(X, Y, Z);
+        }
+
+        public static int GetObjectScale(double X, double Y, double Z)
+        {
+            return GetObjectScale(X, Y, Z);
+        }
+        public static int SetObjectScale(int X, int Y, int Z)
+        {
+            return SetObjectScale(X, Y, Z);
+        }
+    }
+}
